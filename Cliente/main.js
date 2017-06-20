f2e62c24f7915e1bdbe3305a818fa9acd2c1896f
Mitch Aguilar <madialeja06@gmail.com>
2017-06-20 03:51:56 -0500

    prueba conncecciones

---
+++ Cliente/main.js
@@ -0,0 +1,1 @@
+var socket=io.connect('http://186.116.247.89:6677',{'forceNew':true});
--- Server/index.js
+++ Server/index.js
@@ -3,8 +3,17 @@ var app =express();
 var server=require('http').Server(app);
 var io=require('socket.io')(server);
 
-app.get('/holaMundo',function(req,res){//por ruta
+app.use(express.static('Cliente'));
+
+app.get('/holaMundo',function(req,res){//por ruta http://localhost:6677/HolaMundo
     res.status(200).send('hola  mundo');
+});
+app.get('/MitchAguilar',function(req,res){
+    res.status(200).send('<h1 style="color:red;">Not more</h1>');
+});
+io.on('connection',function(socket){
+    console.log('El nodo de ip: '+socket.handshake.address+" se vinculó");
+
 });
 
 server.listen(6677, function(){//lev el server en el 8080 ojo!

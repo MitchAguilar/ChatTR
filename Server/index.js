efef0e5c73ab95076e3b46d6dbc093f9ee69e3d5
Mitch Aguilar <madialeja06@gmail.com>
2017-06-22 01:06:57 -0500

    ende

--- Server/index.js
+++ Server/index.js
@@ -11,10 +11,26 @@ app.get('/holaMundo',function(req,res){//por ruta http://localhost:6677/HolaMund
 app.get('/MitchAguilar',function(req,res){
     res.status(200).send('<h1 style="color:red;">Not more</h1>');
 });
+var messages=[{
+    id:1,
+    text:'bienvendo al chat online de mitch aguilar',
+    nickname:'Bot Mitch Aguilar'
+}];
+
+
+
 io.on('connection',function(socket){
     console.log('El nodo de ip: '+socket.handshake.address+" se vinculó...");
+    socket.emit('messages',messages);
+
+    socket.on('add-message',function(data){
+        messages.push(data);
+
+        io.sockets.emit('messages',messages);
+    });
 
 });
+
 
 server.listen(6677, function(){//lev el server en el 8080 ojo!
     console.log('Servidor está funcionando en http//localhost:6677');
